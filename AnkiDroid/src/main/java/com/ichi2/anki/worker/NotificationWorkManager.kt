f56744be30b3fd66651c8e11b51fefd9e8997c72
Prateek Singh <76490368+prateek-singh-3212@users.noreply.github.com>
2022-06-28 15:44:24 +0530

    Implemented NotificationWorkManager Logic.

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/worker/NotificationWorkManager.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2022 Prateek Singh <prateeksingh3212@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ichi2.anki.worker
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.ichi2.anki.*
+import com.ichi2.libanki.Collection
+import com.ichi2.libanki.sched.Counts
+import com.ichi2.libanki.sched.DeckDueTreeNode
+import com.ichi2.libanki.utils.TimeManager
+import kotlinx.coroutines.flow.firstOrNull
+import timber.log.Timber
+import java.util.*
+
+/**
+ * Worker class to collect the data for notification and triggers all the notifications.
+ * **NOTE: It is a coroutine worker i.e it will run asynchronously on the Dispatcher.DEFAULT**
+ * */
+class NotificationWorkManager(val context: Context, workerParameters: WorkerParameters) :
+    CoroutineWorker(context, workerParameters) {
+
+    // Lambda to check the collection is not null.
+    private val collection: (context: Context) -> Collection? = {
+        val collection = CollectionHelper.getInstance().getColSafe(it)
+        if (collection != null) {
+            collection
+        } else {
+            Timber.d("Unable to access collection.")
+            null
+        }
+    }
+
+    override suspend fun doWork(): Result {
+        Timber.d("NotificationManagerWorker: Worker status -> STARTED")
+        val currentTime = TimeManager.time.currentDate.time
+
+        // Update the data of Deck Metadata from HERE
+        val sched = collection(context)?.sched ?: return Result.failure()
+        val deckList: List<DeckDueTreeNode> = sched.deckDueTree().map { it.value }
+
+        // Collect all the notification data which need to triggered.
+        val timeDeckData = NotificationDatastore.getInstance(context).getTimeDeckData()
+        if (timeDeckData.isNullOrEmpty()) {
+            Timber.d("No time deck data found, returning")
+            fireAllDeckNotification(deckList)
+            return Result.success()
+        }
+
+        // Sorted all the decks whose notification time is less than current time.
+        val filteredTimeDeckData = timeDeckData.filter { it.key.toLong() < currentTime }
+
+        // Creating list of all decks whose notification is going to trigger
+        val deckIdsToTrigger = filteredTimeDeckData.flatMap { it.value }
+
+        // TODO: Cancel deck notification when user deletes a particular deck to handle case [user deletes a deck between the notification being added and executed]
+        deckIdsToTrigger.forEach { deckId ->
+            val did = deckList.firstOrNull { it.did == deckId }
+            // Handles case when deck is deleted.
+            if (did != null) {
+                fireDeckNotification(did)
+            }
+        }
+
+        fireAllDeckNotification(deckList)
+
+        Timber.d("NotificationManagerWorker: Worker status -> FINISHED")
+        return Result.success() // Done work successfully...
+    }
+
+    private fun fireDeckNotification(deckDueTreeNode: DeckDueTreeNode) {
+        Timber.d("Firing deck notification for did -> ${deckDueTreeNode.did}")
+        val title = context.getString(R.string.reminder_title)
+        val counts = Counts(deckDueTreeNode.newCount, deckDueTreeNode.lrnCount, deckDueTreeNode.revCount)
+        val message = context.resources.getQuantityString(
+            R.plurals.reminder_text,
+            counts.count(),
+            deckDueTreeNode.fullDeckName
+        )
+
+        // TODO: Check the minimum no. of cards to send notification.
+        // TODO: Build and fire notification.
+    }
+
+    private fun fireAllDeckNotification(deckList: List<DeckDueTreeNode>) {
+        Timber.d("Firing all deck notification.")
+        val preferences = AnkiDroidApp.getSharedPrefs(context)
+        val minCardsDue = preferences.getString(
+            Preferences.MINIMUM_CARDS_DUE_FOR_NOTIFICATION,
+            Preferences.PENDING_NOTIFICATIONS_ONLY.toString()
+        )!!.toInt()
+
+        // All Decks Notification.
+        val totalDueCount = Counts()
+        deckList.forEach {
+            totalDueCount.addLrn(it.lrnCount)
+            totalDueCount.addNew(it.newCount)
+            totalDueCount.addRev(it.revCount)
+        }
+
+        if (totalDueCount.count() > minCardsDue) {
+            // TODO: Build & Fire all deck notification.
+        }
+    }
+
+    companion object {
+        const val ONE_HOUR_MS = 3600000
+        const val ONE_DAY_MS = 86400000
+
+        fun getTriggerTime(data: allTimeAndDecksMap): Long {
+            val currentTime = TimeManager.time.currentDate.time
+
+            val nextTimeKey = data.keys.firstOrNull { it.toLong() >= currentTime }
+                ?: return currentTime + ONE_HOUR_MS // No deck for complete day. Restarting after 1 hour for all deck notification
+
+            val timeDiff = nextTimeKey.toLong() - currentTime
+
+            return if (timeDiff < ONE_HOUR_MS) {
+                nextTimeKey.toLong()
+            } else {
+                // No deck is scheduled in next hour. Restart service after 1 hour.
+                currentTime + ONE_HOUR_MS
+            }
+        }
+    }
+}
